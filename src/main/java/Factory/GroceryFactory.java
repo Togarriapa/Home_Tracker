dcb127eef9fe487ccbb91a0247959013fe2a3cc1
Tomas_Fonseca <tomas.g.fonseca@gmail.com>
2023-03-29 22:02:18 +0100

    Created and implemented the abstract groceries class and implemented the GroceriesFactory

--- src/main/java/Factory/GroceryFactory.java
+++ src/main/java/Factory/GroceryFactory.java
@@ -1,4 +1,17 @@
 package Factory;
 
+import Grocerys.*;
+
 public class GroceryFactory {
+
+    public static Grocery createGrocery(PerishableCat isItPerishable){
+        switch(isItPerishable){
+            case PERISHABLE:
+                return new FoodItem();
+            case NON_PERISHABLE:
+                return new Item();
+            default:
+                return null;
+        }
+    }
 }
--- src/main/java/Grocerys/Grocery.java
+++ src/main/java/Grocerys/Grocery.java
@@ -1,4 +1,34 @@
 package Grocerys;
 
+import java.sql.Date;
+
 public abstract class Grocery {
+
+    protected String name;
+    protected GrocerysCat category;
+    protected Date expirationDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GrocerysCat getCategory() {
+        return category;
+    }
+
+    public void setCategory(GrocerysCat category) {
+        this.category = category;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
