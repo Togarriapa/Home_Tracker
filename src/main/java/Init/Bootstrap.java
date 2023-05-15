903e027e8ed45bc3952a1f7919e8a3d27d478c6a
Tomas_Fonseca <tomas.g.fonseca@gmail.com>
2023-05-15 09:20:43 +0100

    Better Version 2.0

--- src/main/java/Init/Bootstrap.java
+++ src/main/java/Init/Bootstrap.java
@@ -5,14 +5,13 @@ import Factory.HouseFactory;
 import HouseBuild.House;
 import org.academiadecodigo.bootcamp.Prompt;
 import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
-
 import java.util.Scanner;
 
 public class Bootstrap {
     private Prompt prompt;
     private Scanner scanner;
+    private Boolean firstStarted = true;
     private Boolean isLoggedOn;
-    private Boolean firstStarted;
     private String password;
     private House house;
 
@@ -20,13 +19,11 @@ public class Bootstrap {
 
         prompt = new Prompt(System.in, System.out);
         scanner = new Scanner(System.in);
-
-        firstStarted= true;
-
+        isLoggedOn = false;
         runHouse();
 
     }
-    public void runHouse() {
+    private void runHouse() {
 
         if (firstStarted) {
 
@@ -42,75 +39,75 @@ public class Bootstrap {
 
     }
 
+    private void firstStart(){
 
-        private void firstStart(){
+        house = HouseFactory.createNewBlankHouse();
 
-            house = HouseFactory.createNewBlankHouse();
+        setFirstStarted();
 
-            setFirstStarted();
+    }
 
-        }
+    private void startMenu() {
 
-        public void startMenu() {
+        MenuInputScanner scanner = new MenuInputScanner(Menus.startMenu);
+        scanner.setMessage("****************************************\n" + "Choose your option:");
 
-            MenuInputScanner scanner = new MenuInputScanner(Menus.startMenu);
-            scanner.setMessage("****************************************\n" + "Choose your option:");
+        int answerIndex = prompt.getUserInput(scanner);
+        System.out.println("****************************************");
+        getResultStartMenu(answerIndex);
 
-            int answerIndex = prompt.getUserInput(scanner);
-            System.out.println("****************************************");
-            getResultStartMenu(answerIndex);
+    }
 
-        }
+    private void getResultStartMenu(int answerIndex){
 
-        private void getResultStartMenu(int answerIndex){
+        switch (answerIndex) {
 
-            switch (answerIndex) {
-
-                case 1:
-                    System.out.println("You choose: LogIn");
-                    LogIn();
-
-                    break;
-                case 2:
-                    System.out.println("You choose: Reset Home");
-                    resetHome();
-                    break;
-            }
-
-        }
-
-        private void LogIn() {
-            System.out.println("****************************************\n" + "Type your password:");
-            String answerIndex = scanner.nextLine();
-
-            if(answerIndex.equals(password)){
-
-                System.out.println("****************************************\n" + "Welcome Home");
-                house.run();
-
-            }else{
-
+            case 1:
+                System.out.println("You choose: LogIn");
                 LogIn();
 
-            }
+                break;
+            case 2:
+                System.out.println("You choose: Reset Home");
+                resetHome();
+                break;
+        }
+
+    }
+
+    private void LogIn() {
+        System.out.println("****************************************\n" + "Type your password:");
+        String answerIndex = scanner.nextLine();
+
+        if(answerIndex.equals(password)){
+
+            System.out.println("****************************************\n" + "Welcome Home");
+            Boolean isLoggedOn = true;
+            house.run();
+
+        }else{
+            System.out.println("****************************************\n" + "Password Incorrect");
+            LogIn();
 
         }
 
-        private void resetHome() {
+    }
 
-            house = HouseFactory.createNewBlankHouse();
+    private void resetHome() {
 
-        }
+        house = HouseFactory.createNewBlankHouse();
 
-        private void setFirstStarted(){
+    }
 
-            System.out.println("****************************************\n" + "Choose your password:");
-            String answerIndex = scanner.nextLine();
-            System.out.println("****************************************");
-            this.password = answerIndex;
-            firstStarted = false;
+    private void setFirstStarted(){
 
-        }
+        System.out.println("****************************************\n" + "Choose your password:");
+        String answerIndex = scanner.nextLine();
+        System.out.println("****************************************");
+        this.password = answerIndex;
+        firstStarted = false;
+
+    }
 
 
 }
